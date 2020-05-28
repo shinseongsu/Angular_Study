ecbe2b0d8a0327baa749b75fe3758f13565d861b
Shin seongsu <tlstjdtn321@naver.com>
2020-05-28 20:55:20 +0900

    angular 목록

---
+++ ch03/how-angular-works/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductRowComponent,
+    ProductImageComponent,
+    PriceDisplayComponent,
+    ProductDepartmentComponent,
+    ProductsListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ch03/how-angular-works/src/app/product-department/product-department.component.ts
@@ -0,0 +1,14 @@
+import { 
+  Component,
+  Input 
+} from '@angular/core';
+
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'product-department',
+  templateUrl: './product-department.component.html'
+})
+export class ProductDepartmentComponent {
+  @Input() product: Product;
+}
