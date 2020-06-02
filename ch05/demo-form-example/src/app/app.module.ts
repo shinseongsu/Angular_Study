427345192e71df987e1e0743ef2f4c19b7cc92c0
Shin seongsu <tlstjdtn321@naver.com>
2020-06-02 21:12:40 +0900

    폼 체크

---
+++ ch05/demo-form-example/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DemoFormSkuComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
