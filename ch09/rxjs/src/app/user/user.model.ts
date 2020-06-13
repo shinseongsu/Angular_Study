e70b81ba2f3d50ba9386465681ed32f22784f67d
Shin seongsu <tlstjdtn321@naver.com>
2020-06-13 21:48:56 +0900

    user

---
+++ ch09/rxjs/src/app/user/user.model.ts
@@ -0,0 +1,10 @@
+import { uuid } from '../util/uuid';
+
+export class User {
+  id: string;
+
+  constructor(public name: string,
+              public avatarSrc: string) {
+    this.id = uuid();
+  }
+}
